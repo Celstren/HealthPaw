4dda863876408f63dbe484a5e288a0c1bfc10bac
Gustavo Carrillo <gustavo.carrillo.rueda10@gmail.com>
2020-09-12 18:10:25 -0500

    remove unused code

--- lib/utils/widgets/stats_field.dart
+++ lib/utils/widgets/stats_field.dart
@@ -1,4 +1,5 @@
 import 'package:HealthPaw/models/pet/stadistic.dart';
+/// Example of a simple line chart.
 import 'package:charts_flutter/flutter.dart' as charts;
 import 'package:flutter/material.dart';
 
@@ -24,4 +25,62 @@ class StatsField extends StatelessWidget {
   Widget build(BuildContext context) {
     return new charts.LineChart(_castData(data), animate: animate);
   }
+}
+
+class SimpleLineChart extends StatelessWidget {
+  final List<charts.Series> seriesList;
+  final bool animate;
+
+  SimpleLineChart(this.seriesList, {this.animate});
+
+  /// Creates a [LineChart] with sample data and no transition.
+  factory SimpleLineChart.withSampleData() {
+    return new SimpleLineChart(
+      _createSampleData(),
+      // Disable animations for image tests.
+      animate: false,
+    );
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return new charts.LineChart(seriesList, animate: animate);
+  }
+
+  /// Create one series with sample hard coded data.
+  static List<charts.Series<LinearSales, int>> _createSampleData() {
+    final data = [
+      new LinearSales(0, 5),
+      new LinearSales(1, 25),
+      new LinearSales(2, 100),
+      new LinearSales(3, 75),
+    ];
+
+    return [
+      new charts.Series<LinearSales, int>(
+        id: 'Sales',
+        colorFn: (_, __) => charts.MaterialPalette.blue.shadeDefault,
+        domainFn: (LinearSales sales, _) => sales.year,
+        measureFn: (LinearSales sales, _) => sales.sales,
+        data: data,
+      )
+    ];
+  }
+}
+
+/// Sample linear data type.
+class LinearSales {
+  final int year;
+  final int sales;
+
+  LinearSales(this.year, this.sales);
+}
+
+/// Sample linear data type.
+class StatsData {
+  final int minimum;
+  final int maximum;
+
+  StatsData(this.minimum, this.maximum);
 }
